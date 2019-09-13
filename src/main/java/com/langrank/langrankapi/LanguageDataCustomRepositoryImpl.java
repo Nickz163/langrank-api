9609d25666cec77d4ea8d9dc622677f78d4362dd
Your Name <your@email.address>
2019-09-13 15:56:00 +0300

    deleted deprecated Controllers

--- src/main/java/com/langrank/langrankapi/LanguageDataCustomRepositoryImpl.java
+++ src/main/java/com/langrank/langrankapi/LanguageDataCustomRepositoryImpl.java
@@ -6,8 +6,6 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
-
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
